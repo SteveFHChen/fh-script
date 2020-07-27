ed283bb202d27d242ecccbe2b1956e3088825734
SteveFHChen <chenfanghui2006741@126.com>
2020-07-28 00:14:32 +0800

    Add new function - predict with sk learn

--- pythonsc/webcat/covidsklearn.py
+++ pythonsc/webcat/covidsklearn.py
@@ -8,6 +8,25 @@ import matplotlib.pyplot as plt
 
 from pymysql import *
 
+import sys
+import time
+import json
+
+#Sample command:
+#success:
+#python C:/fh/ws/ws1/fh-script/pythonsc/webcat/covidsklearn.py "{'area': '美国', 'outputPath': 'C:/fh/testenv1/chart','diagramFileName':'plothello20200725-185829.png'}"
+
+
+param1=sys.argv[1]
+#myprint_debug("param1: ", type(param1), param1);
+params = json.loads(param1.replace("'", "\""));
+
+area=params['area'];
+diagramFileName = params['diagramFileName']
+picPath=params['outputPath'];
+
+fileName = diagramFileName[:len(diagramFileName)-4]+'-2'+diagramFileName[-4:]
+
 conn = connect(host='localhost', port=3306, database='test',
                user='root',
                password='root123', charset='utf8')
@@ -16,7 +35,8 @@ cs1 = conn.cursor()
 input1 = 'covid_area_stat'
 input2_1 = 'total_confirmed'
 input2_2 = 'new_confirmed'
-input3 = '南非'
+#input3 = '南非'
+input3 = area;
 input4 = "business_date"
 input4_1 = "DATE_FORMAT(business_date, '%Y%m%d') business_date"
 
@@ -61,6 +81,7 @@ y_predict=knn.predict(x_test)
 
 xindex = np.arange(len(x_test));
 
+'''
 plt.subplot(2,2,1);
 plt.plot(xindex, y_predict, label='Predict', color='green');
 plt.title('Predict Result');
@@ -71,12 +92,22 @@ plt.plot(xindex, y_test, label='Line', color='blue', linewidth=2.0, linestyle='d
 plt.scatter(xindex, y_test, label='Real', color='red');
 plt.title('Real Result');
 plt.legend(loc='upper left')
+'''
 
-plt.subplot(2,2,3);
+#plt.subplot(2,2,3);
+plt.subplot(1,1,1);
+
 plt.plot(xindex, y_predict, label='Predict', color='green');
+plt.scatter(xindex, y_predict, label='Predict', color='green');
+
 plt.plot(xindex, y_test, label='Line', color='blue', linewidth=2.0, linestyle='dotted');
 plt.scatter(xindex, y_test, label='Real', color='red');
+
 plt.title('Compare Predict and Real Result');
 plt.legend(loc='upper left')
 
-plt.show();
+plt.savefig(picPath+'/'+fileName,bbox_inches='tight')
+#plt.show()
+
+params['diagramFileName']=fileName;
+print(json.dumps(params));
