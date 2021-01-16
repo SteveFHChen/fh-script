ad095db857f684637a352e63ff88bb3ffb8ddffc
SteveFHChen <chenfanghui2006741@126.com>
2021-01-16 17:25:01 +0800

    update backup sql

--- pythonsc/data/backup_data.sql
+++ pythonsc/data/backup_data.sql
@@ -6,8 +6,14 @@ CREATE TABLE covid_area_stat2008 AS SELECT * FROM covid_area_stat WHERE business
 CREATE TABLE covid_area_stat2009 AS SELECT * FROM covid_area_stat WHERE business_date >='2020-09-01 00:00:00' AND business_date<'2020-10-01 00:00:00';
 CREATE TABLE covid_area_stat2010 AS SELECT * FROM covid_area_stat WHERE business_date >='2020-10-01 00:00:00' AND business_date<'2020-11-01 00:00:00';
 CREATE TABLE covid_area_stat2011 AS SELECT * FROM covid_area_stat WHERE business_date >='2020-11-01 00:00:00' AND business_date<'2020-12-01 00:00:00';
+CREATE TABLE covid_area_stat2012 AS SELECT * FROM covid_area_stat WHERE business_date >='2020-12-01 00:00:00' AND business_date<'2021-01-01 00:00:00';
 
 --Pending action
-CREATE TABLE covid_area_stat2012 AS SELECT * FROM covid_area_stat WHERE business_date >='2020-12-01 00:00:00' AND business_date<'2021-01-01 00:00:00';
+CREATE TABLE covid_area_stat2101 AS SELECT * FROM covid_area_stat WHERE business_date >='2021-01-01 00:00:00' AND business_date<'2021-02-01 00:00:00';
+CREATE TABLE covid_area_stat2102 AS SELECT * FROM covid_area_stat WHERE business_date >='2021-02-01 00:00:00' AND business_date<'2021-03-01 00:00:00';
+CREATE TABLE covid_area_stat2103 AS SELECT * FROM covid_area_stat WHERE business_date >='2021-03-01 00:00:00' AND business_date<'2021-04-01 00:00:00';
+CREATE TABLE covid_area_stat2104 AS SELECT * FROM covid_area_stat WHERE business_date >='2021-04-01 00:00:00' AND business_date<'2021-05-01 00:00:00';
+CREATE TABLE covid_area_stat2105 AS SELECT * FROM covid_area_stat WHERE business_date >='2021-05-01 00:00:00' AND business_date<'2021-06-01 00:00:00';
+
 
 
