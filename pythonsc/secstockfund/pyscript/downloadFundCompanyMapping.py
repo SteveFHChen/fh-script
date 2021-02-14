e9e6b5bad67bca0601b07ebd43fb1d3c0f1b8284
SteveFHChen <chenfanghui2006741@126.com>
2021-02-14 17:48:53 +0800

    Securities initialize.

---
+++ pythonsc/secstockfund/pyscript/downloadFundCompanyMapping.py
@@ -0,0 +1,44 @@
+#Capture fund and company mapping, and fill into DB
+from fhsechead import *
+
+driver = webdriver.Chrome(executable_path="C:/fh/testenv1/chromedriver.exe")
+driver.set_page_load_timeout(40)
+
+def captureFundCompanyMapping():
+    sqlCompnayList = "SELECT company_code, company_name FROM sec_fund_company"
+    rowcount = cs1.execute(sqlCompnayList)
+    fundCompanyList = cs1.fetchall()
+    for company in fundCompanyList:
+        #company = fundCompanyList[0]
+        companyCode, companyName = company[0], company[1]
+        try:
+            driver.get(f"http://fund.eastmoney.com/Company/{companyCode}.html") 
+        except:
+            print(f'{time.ctime()} Loading page timeout, company is {company}.')
+        
+        jsFundCompanyMapping = f"""
+            fundCompanyMapping = {{"companyCode":"{companyCode}", "fundList":[]}}
+            document.querySelectorAll("a.code").forEach(function(f, i, a){{
+                fundCompanyMapping.fundList.push(f.innerText);
+            }});
+            return JSON.stringify(fundCompanyMapping);
+            """
+        fundCompanyMapping=json.loads(driver.execute_script(jsFundCompanyMapping))
+        fundListString = "'"+"','".join(fundCompanyMapping['fundList'])+"'"
+        sqlUpdateFundCompanyMapping = f"""
+            UPDATE sec_fund
+            SET company_code = '{companyCode}'
+            WHERE fund_code in ({fundListString})
+            """
+        rowcount = cs1.execute(sqlUpdateFundCompanyMapping)
+        conn.commit()
+        print(f"{time.ctime()} company {companyCode}-{companyName} has {len(fundCompanyMapping['fundList'])} funds in internet, updated {rowcount} funds in DB.")
+
+    print(f"{time.ctime()} Update company:fund mapping for {len(fundCompanyList)} companies completed.")
+
+if __name__ == "__main__":
+    captureFundCompanyMapping()
+    
+    cs1.close()
+    conn.close()
+    driver.close()
---
+++ pythonsc/secstockfund/pyscript/downloadFundStockCurrentValue.py
@@ -0,0 +1,108 @@
+#Capture stock current data and update into DB
+from fhsechead import *
+
+currentDate = time.strftime("%Y%m%d", time.localtime())
+
+def downloadStockCurrent(stockCodeList):
+    stockCodeListStr = ",".join(stockCodeList)
+    #stockCodeList = "0600519,0601318,1000333,0600036" #Sample
+    #url = f"http://api.money.126.net/data/feed/0600519,0601318,1000333,0600036,money.api"
+    url = f"http://api.money.126.net/data/feed/{stockCodeListStr},money.api"
+    
+    try:
+        res = requests.get(url, headers=headers)
+        content = res.text
+        result = json.loads(content[len("_ntes_quote_callback("):-len(");")])
+        
+        #time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        for stockCode in result.keys():
+            #stockCode='1000023'
+            stock = result[stockCode]
+            if 'yestclose' in stock.keys():
+                sqlUpdatePrice = f"""
+                    UPDATE sec_stock
+                    SET price_time='{stock['time']}', yest_close={stock['yestclose']}, now_price={stock['price']}, arrow='{stock['arrow']}', 
+                        open_price={stock['open']}, high_price={stock['high']}, low_price={stock['low']}, volume={stock['volume']}, percent={stock['percent']}
+                    WHERE stock_code='{stockCode[1:]}'
+                    """
+                rowcount = cs1.execute(sqlUpdatePrice)
+            else:
+                print(f"No data - {stock}")
+        conn.commit()
+    except Exception as e:
+        print(f"Exception msg: {e}")
+        return -1
+    
+    return 0
+
+def stockCurrentMain():
+    sqlStockCodeList = """
+        SELECT CONCAT(CASE WHEN SUBSTRING(stock_code, 1, 1) IN ('0','3') THEN 1 ELSE 0 END, stock_code) stock_code 
+        FROM sec_stock t 
+        WHERE region_code='CN' AND price_time IS NULL
+        ORDER BY fund_welcome_cnt DESC
+        """
+    rowcount = cs1.execute(sqlStockCodeList)
+    records = cs1.fetchall()
+    stockCodeList = []
+    for r in records:
+        stockCodeList.append(r[0])
+    batchUnit = 500
+    for i in range(math.ceil(len(stockCodeList)/batchUnit)):
+        print(f"Update stock price for {0+i*batchUnit} - {batchUnit*(i+1)}:")
+        status = downloadStockCurrent(stockCodeList[0+i*batchUnit:batchUnit*(i+1)])
+        print(f"Call downloadStockCurrent() status={status}")
+
+
+#Capture fund current data and update into DB
+def downloadFundCurrent(fundCode):
+    #fundCode = "161725" #Sample
+    url = f"http://fundgz.1234567.com.cn/js/{fundCode}.js"
+    
+    try:
+        res = requests.get(url, headers=headers)
+        content = res.text
+        fund = json.loads(content[len("jsonpgz("):-len(");")])
+        
+        sqlUpdatePrice = f"""
+            UPDATE sec_fund
+            SET net_date='{fund['jzrq']}', unit_net={fund['dwjz']},esti_net={fund['gsz']},esti_rate={fund['gszzl']},gztime='{fund['gztime']}'
+            WHERE fund_code='{fund['fundcode']}'
+            """
+        rowcount = cs1.execute(sqlUpdatePrice)
+        conn.commit()
+    except Exception as e:
+        print(e)
+        return -1
+    
+    return 0
+
+def fundCurrentMain():
+    sqlFundCodeList = """
+        SELECT fund_code 
+        FROM sec_fund
+        ORDER BY fund_code DESC
+        """
+    sqlFundCodeList="""
+        SELECT fund_code FROM (
+        	SELECT @rownum:=@rownum+1 rownum, a.*
+        	FROM sec_fund a, (SELECT @rownum:=0) b
+        	WHERE fh_mark IS NOT NULL AND fund_type NOT IN ('ETF-场内', '债券型') 
+        	ORDER BY fund_scale DESC
+        ) t WHERE rownum<=10
+        """
+    rowcount = cs1.execute(sqlFundCodeList)
+    records = cs1.fetchall()
+    fundCodeList = []
+    for r in records:
+        fundCodeList.append(r[0])
+    for fundCode in fundCodeList:
+        status = downloadFundCurrent(fundCode)
+        print(f"Call downloadStockCurrent() status={status}")
+
+if __name__ == "__main__":
+    stockCurrentMain()
+    fundCurrentMain()
+    
+    cs1.close()
+    conn.close()
